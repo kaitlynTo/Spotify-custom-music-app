6cbc9a51bbe61c87a5e39c271177ac7d1c043638
KaitlynTo <juyoung.toronto@gmail.com>
2022-02-01 10:38:35 -0500

    edit2

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  clientID: "81641a93c5a4439cb0e0a120630e5dbd",
-  clientSecret: "0fe7bbf4f1ad47a09286a82e0e8df084",
-  userAPIBase: "https://intense-garden-14479.herokuapp.com/api/user"
-};
